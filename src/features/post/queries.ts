6a274935eba478d77b203a58a0222d3183988751
Андрій Шевельов <ashevelevmti@gmail.com>
2026-03-26 13:26:44 +0200

    fix db

--- src/features/post/queries.ts
+++
@@ -1,7 +0,0 @@
-/** @format */
-"use server";
-import { cache } from "react";
-import { safeQuery } from "@/utils/wrapperQuery";
-import { postService } from "./services";
-
-export const getPosts = cache(() => safeQuery(postService.get));
