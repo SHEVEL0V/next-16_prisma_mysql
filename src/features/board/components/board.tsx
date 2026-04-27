e14bd85ded7369eca0c9cea98d80bd9b4be582f0
Андрій Шевельов <ashevelevmti@gmail.com>
2026-04-27 12:07:08 +0300

    fix

--- src/features/board/components/board.tsx
+++ src/features/board/components/board.tsx
@@ -2,33 +2,33 @@
 
 // features/board/components/board-canvas.tsx
 import { Box } from "@mui/material";
-import Sidebar from "@/features/board/components/sidebar/Sidebar";
-import { getBoards, getBoardById } from "@/features/board/queries";
 import CenteredMessage from "@/components/ui/CenteredMessage";
 import DragDropWrapper from "@/features/board/components/DragDrop";
+import Sidebar from "@/features/board/components/sidebar/Sidebar";
+import { getBoardById, getBoards } from "@/features/board/queries";
 
 export default async function Board({ boardId }: { boardId?: string }) {
-	const boards = (await getBoards())?.data ?? [];
+  const boards = (await getBoards())?.data ?? [];
 
-	const activeBoard = boardId || boards[0]?.id;
+  const activeBoard = boardId || boards[0]?.id;
 
-	if (!activeBoard) {
-		return (
-			<CenteredMessage message="Будь ласка, створіть або виберіть дошку." />
-		);
-	}
+  if (!activeBoard) {
+    return (
+      <CenteredMessage message="Будь ласка, створіть або виберіть дошку." />
+    );
+  }
 
-	const boardResponse = await getBoardById(activeBoard!);
-	const board = boardResponse?.data;
+  const boardResponse = await getBoardById(activeBoard);
+  const board = boardResponse?.data;
 
-	return (
-		<Box sx={{ display: "flex" }}>
-			<Sidebar boards={boards} activeBoard={activeBoard} />
-			{board ? (
-				<DragDropWrapper boardId={activeBoard} initialData={board.columns} />
-			) : (
-				<CenteredMessage message="Дошку не знайдено або сталася помилка." />
-			)}
-		</Box>
-	);
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Sidebar boards={boards} activeBoard={activeBoard} />
+      {board ? (
+        <DragDropWrapper boardId={activeBoard} initialData={board.columns} />
+      ) : (
+        <CenteredMessage message="Дошку не знайдено або сталася помилка." />
+      )}
+    </Box>
+  );
 }
